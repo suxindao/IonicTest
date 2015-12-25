bf52e06282bc285a7f64d5228c5ff2e28e4bca43
suxindao <suxindao@126.com>
2015-12-25 22:36:40 +0800

    add pluginTestApp update wgz_blank

---
+++ pluginTest/www/js/app.js
@@ -0,0 +1,82 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic','ionic.service.core', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+        $ionicPlatform.ready(function() {
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+                // for form inputs)
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+                // Don't remove this line unless you know what you are doing. It stops the viewport
+                // from snapping when text inputs are focused. Ionic handles this internally for
+                // a much nicer keyboard experience.
+                cordova.plugins.Keyboard.disableScroll(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    })
+    .config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+
+        $ionicConfigProvider.platform.ios.tabs.style('standard');
+        $ionicConfigProvider.platform.ios.tabs.position('bottom');
+        $ionicConfigProvider.platform.android.tabs.style('standard');
+        $ionicConfigProvider.platform.android.tabs.position('standard');
+
+        $ionicConfigProvider.platform.ios.navBar.alignTitle('center');
+        $ionicConfigProvider.platform.android.navBar.alignTitle('left');
+
+        $ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
+        $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');
+
+        $ionicConfigProvider.platform.ios.views.transition('ios');
+        $ionicConfigProvider.platform.android.views.transition('android');
+
+        $stateProvider
+        // setup an abstract state for the tabs directive
+            .state('tab', {
+            url: '/tab',
+            abstract: true,
+            templateUrl: 'templates/tabs.html'
+        })
+
+        // Each tab has its own nav history stack:
+
+        .state('tab.analytics', {
+                url: '/analytics',
+                views: {
+                    'tab-analytics': {
+                        templateUrl: 'templates/tab-analytics.html',
+                        controller: 'AnalyticsCtrl'
+                    }
+                }
+            })
+            .state('tab.deploy', {
+                url: '/deploy',
+                views: {
+                    'tab-deploy': {
+                        templateUrl: 'templates/tab-deploy.html',
+                        controller: 'DeployCtrl'
+                    }
+                }
+            })
+            .state('tab.push', {
+                url: '/push',
+                views: {
+                    'tab-push': {
+                        templateUrl: 'templates/tab-push.html',
+                        controller: 'PushCtrl'
+                    }
+                }
+            });
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/tab/analytics');
+
+    });
---
+++ pluginTest/www/js/controllers.js
@@ -0,0 +1,31 @@
+angular.module('starter.controllers', [])
+
+.controller('AnalyticsCtrl', function($scope) {})
+
+.controller('DeployCtrl', function($scope) {
+    var deploy = new Ionic.Deploy();
+
+    // Update app code with new release from Ionic Deploy
+    $scope.doUpdate = function() {
+        deploy.update().then(function(res) {
+            console.log('Ionic Deploy: Update Success! ', res);
+        }, function(err) {
+            console.log('Ionic Deploy: Update error! ', err);
+        }, function(prog) {
+            console.log('Ionic Deploy: Progress... ', prog);
+        });
+    };
+
+    // Check Ionic Deploy for new code
+    $scope.checkForUpdates = function() {
+        console.log('Ionic Deploy: Checking for updates');
+        deploy.check().then(function(hasUpdate) {
+            console.log('Ionic Deploy: Update available: ' + hasUpdate);
+            $scope.hasUpdate = hasUpdate;
+        }, function(err) {
+            console.error('Ionic Deploy: Unable to check for updates', err);
+        });
+    }
+})
+
+.controller('PushCtrl', function($scope) {});
