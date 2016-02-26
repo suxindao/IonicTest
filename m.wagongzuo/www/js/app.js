c7bb8e6be27ec161d9bc9d0f2a4373a8d00491ed
suxindao <suxindao@126.com>
2016-02-26 20:38:12 +0800

    test

--- m.wagongzuo/www/js/app.js
+++ m.wagongzuo/www/js/app.js
@@ -7,126 +7,126 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-        .run(function ($ionicPlatform) {
-            $ionicPlatform.ready(function () {
-                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-                // for form inputs)
-                if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-                    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-                    cordova.plugins.Keyboard.disableScroll(true);
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
 
-                }
-                if (window.StatusBar) {
-                    // org.apache.cordova.statusbar required
-                    StatusBar.styleDefault();
-                }
-            });
-        })
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
 
-        .config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
-            
-            //用来将tab标签固定在底部
-            $ionicConfigProvider.platform.ios.tabs.style('standard');
-            $ionicConfigProvider.platform.ios.tabs.position('bottom');
-            $ionicConfigProvider.platform.android.tabs.style('standard');
-            $ionicConfigProvider.platform.android.tabs.position('standard');
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
 
-            $ionicConfigProvider.platform.ios.navBar.alignTitle('center');
-            $ionicConfigProvider.platform.android.navBar.alignTitle('left');
+  //用来将tab标签固定在底部
+  $ionicConfigProvider.platform.ios.tabs.style('standard');
+  $ionicConfigProvider.platform.ios.tabs.position('bottom');
+  $ionicConfigProvider.platform.android.tabs.style('standard');
+  $ionicConfigProvider.platform.android.tabs.position('standard');
 
-            $ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
-            $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');
+  $ionicConfigProvider.platform.ios.navBar.alignTitle('center');
+  $ionicConfigProvider.platform.android.navBar.alignTitle('left');
 
-            $ionicConfigProvider.platform.ios.views.transition('ios');
-            $ionicConfigProvider.platform.android.views.transition('android');
+  $ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
+  $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');
 
-            // Ionic uses AngularUI Router which uses the concept of states
-            // Learn more here: https://github.com/angular-ui/ui-router
-            // Set up the various states which the app can be in.
-            // Each state's controller can be found in controllers.js
-            $stateProvider
+  $ionicConfigProvider.platform.ios.views.transition('ios');
+  $ionicConfigProvider.platform.android.views.transition('android');
 
-                    // setup an abstract state for the tabs directive
-                    .state('tab', {
-                        url: '/tab',
-                        abstract: true,
-                        templateUrl: 'templates/tabs.html'
-                    })
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
 
-                    // Each tab has its own nav history stack:
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
 
-                    .state('tab.dash', {
-                        url: '/dash',
-                        views: {
-                            'tab-dash': {
-                                templateUrl: 'templates/tab-dash.html',
-                                controller: 'DashCtrl'
-                            }
-                        }
-                    })
+  // Each tab has its own nav history stack:
 
-                    .state('tab.chats', {
-                        url: '/chats',
-                        views: {
-                            'tab-chats': {
-                                templateUrl: 'templates/tab-chats.html',
-                                controller: 'ChatsCtrl'
-                            }
-                        }
-                    })
-                    .state('tab.chat-detail', {
-                        url: '/chats/:chatId',
-                        views: {
-                            'tab-chats': {
-                                templateUrl: 'templates/chat-detail.html',
-                                controller: 'ChatDetailCtrl'
-                            }
-                        }
-                    })
+  .state('tab.dash', {
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
 
-                    .state('tab.account', {
-                        url: '/account',
-                        views: {
-                            'tab-account': {
-                                templateUrl: 'templates/tab-account.html',
-                                controller: 'AccountCtrl'
-                            }
-                        }
-                    })
+  .state('tab.chats', {
+      url: '/chats',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/tab-chats.html',
+          controller: 'ChatsCtrl'
+        }
+      }
+    })
+    .state('tab.chat-detail', {
+      url: '/chats/:chatId',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/chat-detail.html',
+          controller: 'ChatDetailCtrl'
+        }
+      }
+    })
 
-                    .state('tab.home', {
-                        url: '/home',
-                        views: {
-                            'tab-home': {
-                                templateUrl: 'templates/tab-home.html',
-                                controller: 'HomeCtrl'
-                            }
-                        }
-                    })
+  .state('tab.account', {
+    url: '/account',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
+      }
+    }
+  })
 
-                    .state('result', {
-                        url: '/result/:keyword',
-                        templateUrl: 'templates/result.html',
-                        controller: 'ResultCtrl'
-                    })
+  .state('tab.home', {
+    url: '/home',
+    views: {
+      'tab-home': {
+        templateUrl: 'templates/tab-home.html',
+        controller: 'HomeCtrl'
+      }
+    }
+  })
 
-                    .state('jinfo', {
-                        url: '/jinfo/jid/:jid/cid/:cid',
-                        templateUrl: 'templates/jinfo.html',
-                        controller: 'JobInfoCtrl'
-                    })
+  .state('result', {
+    url: '/result/:keyword',
+    templateUrl: 'templates/result.html',
+    controller: 'ResultCtrl'
+  })
 
-                    .state('home', {
-                        url: '/home',
-                        views: {
-                            '': {
-                                templateUrl: 'templates/tab-home.html',
-                                controller: 'HomeCtrl'
-                            }
-                        }
-                    });
+  .state('jinfo', {
+    url: '/jinfo/jid/:jid/cid/:cid',
+    templateUrl: 'templates/jinfo.html',
+    controller: 'JobInfoCtrl'
+  })
 
-            // if none of the above states are matched, use this as the fallback
-            $urlRouterProvider.otherwise('/tab/home');
+  .state('home', {
+    url: '/home',
+    views: {
+      '': {
+        templateUrl: 'templates/tab-home.html',
+        controller: 'HomeCtrl'
+      }
+    }
+  });
 
-        });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/home');
+
+});
--- m.wagongzuo/www/js/controllers.js
+++ m.wagongzuo/www/js/controllers.js
@@ -1,105 +1,112 @@
 angular.module('starter.controllers', ['ionic'])
 
-        .controller('HomeCtrl', function ($scope, $state) {
-            $scope.search = {};
-            $scope.getResult = function (keyword) {
-                $state.go("result", {keyword: keyword});
-            };
-        })
+.controller('HomeCtrl', function($scope, $state) {
+  $scope.search = {};
+  $scope.getResult = function(keyword) {
+    console.log($scope.search.keyword);
+    $state.go("result", {
+      keyword: $scope.search.keyword
+    });
+  };
+})
 
-        .controller('DashCtrl', function ($scope) {})
+.controller('DashCtrl', function($scope) {})
 
-        .controller('ResultCtrl', function ($scope, $state, $stateParams, $ionicLoading, $ionicHistory, AjaxRequest) {
-//            $scope.resultItems = {};
-            var keyword = $stateParams.keyword || "php", page = 1;
-//            $scope.resultItems = GjResult.search(keyword);
-//            alert("jobs:" + $scope.resultItems.jobs.length);
+.controller('ResultCtrl', function($scope, $state, $stateParams, $ionicLoading, $ionicHistory, AjaxRequest) {
+  //            $scope.resultItems = {};
+  var keyword = $stateParams.keyword || "php",
+    page = 1;
+  //            $scope.resultItems = GjResult.search(keyword);
+  //            alert("jobs:" + $scope.resultItems.jobs.length);
 
-            //获取结果列表
-            $ionicLoading.show({
-                template: "正在载入数据，请稍后..."
-            });
-            AjaxRequest.getResultList(keyword, page).success(function (response, status) {
-                $scope.resultItems = response;
-                $ionicLoading.hide();
-            }).error(function (response, status) {
+  //获取结果列表
+  $ionicLoading.show({
+    template: "正在载入数据，请稍后..."
+  });
+  AjaxRequest.getResultList(keyword, page).success(function(response, status) {
+    $scope.resultItems = response;
+    $ionicLoading.hide();
+  }).error(function(response, status) {
 
-            });
+  });
 
-            $scope.getMore = function () {
-                page++;
-                $ionicLoading.show({
-                    template: "正在载入数据，请稍后..."
-                });
-                AjaxRequest.getResultList(keyword, page).success(function (response, status) {
-                    $scope.resultItems.jobs = $scope.resultItems.jobs.concat(response.jobs);
-                    $ionicLoading.hide();
-                }).error(function (response, status) {
+  $scope.getMore = function() {
+    page++;
+    $ionicLoading.show({
+      template: "正在载入数据，请稍后..."
+    });
+    AjaxRequest.getResultList(keyword, page).success(function(response, status) {
+      $scope.resultItems.jobs = $scope.resultItems.jobs.concat(response.jobs);
+      $ionicLoading.hide();
+    }).error(function(response, status) {
 
-                });
-            }
+    });
+  }
 
-            //跳转至Job detail页面
-            $scope.detail = function (jid, cid) {
-                $state.go("jinfo", {jid: jid, cid: cid});
-            }
+  //跳转至Job detail页面
+  $scope.detail = function(jid, cid) {
+    $state.go("jinfo", {
+      jid: jid,
+      cid: cid
+    });
+  }
 
-            $scope.goHome = function () {
-//                $state.go("tab.home");
-//                var h = $ionicHistory.viewHistory();
-                $ionicHistory.goBack();
-            };
-        })
+  $scope.goHome = function() {
+    //                $state.go("tab.home");
+    //                var h = $ionicHistory.viewHistory();
+    $ionicHistory.goBack();
+  };
+})
 
-        .controller('JobInfoCtrl', function ($scope, $state, $stateParams, $ionicLoading, $ionicTabsDelegate, AjaxRequest) {
-            var jid = $stateParams.jid,
-                    cid = $stateParams.cid;
+.controller('JobInfoCtrl', function($scope, $state, $stateParams, $ionicLoading, $ionicTabsDelegate, AjaxRequest) {
+  var jid = $stateParams.jid,
+    cid = $stateParams.cid;
 
-            //获取结果列表
-            $ionicLoading.show({
-                template: "正在载入数据，请稍后..."
-            });
-            AjaxRequest.getJobDetail(jid, cid).success(function (response, status) {
-                if (response.success === true) {
-                    $scope.jinfo = response.jobinfo;
-                }
-                $ionicLoading.hide();
-            }).error(function (response, status) {
+  //获取结果列表
+  $ionicLoading.show({
+    template: "正在载入数据，请稍后..."
+  });
+  AjaxRequest.getJobDetail(jid, cid).success(function(response, status) {
+    if (response.success === true) {
+      $scope.jinfo = response.jobinfo;
+    }
+    $ionicLoading.hide();
+  }).error(function(response, status) {
 
-            });
+  });
 
-            $scope.onSwipeRight = function () {
-                $ionicTabsDelegate.select(0);
-            };
+  $scope.onSwipeRight = function() {
+    $ionicTabsDelegate.select(0);
+  };
 
-            $scope.onSwipeLeft = function () {
-                $ionicTabsDelegate.select(1);
-            };
+  $scope.onSwipeLeft = function() {
+    $ionicTabsDelegate.select(1);
+  };
 
-        })
+})
 
-        .controller('ChatsCtrl', function ($scope, Chats) {
-            // With the new view caching in Ionic, Controllers are only called
-            // when they are recreated or on app start, instead of every page change.
-            // To listen for when this page is active (for example, to refresh data),
-            // listen for the $ionicView.enter event:
-            //
-            //$scope.$on('$ionicView.enter', function(e) {
-            //});
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
 
-            $scope.chats = Chats.all();
+  $scope.chats = Chats.all();
 
-            $scope.remove = function (chat) {
-                Chats.remove(chat);
-            };
-        })
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
 
-        .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
-            $scope.chat = Chats.get($stateParams.chatId);
-        })
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
 
-        .controller('AccountCtrl', function ($scope) {
-            $scope.settings = {
-                enableFriends: true
-            };
-        });
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
